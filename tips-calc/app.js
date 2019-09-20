32a78998e80449a7dd38ac5b0df2dd42811ff295
Danny Olsen <dao@milestone.dk>
2019-09-20 11:55:46 +0200

    added tip calculation in function

--- tips-calc/app.js
+++ tips-calc/app.js
@@ -1,3 +1,31 @@
-function calcTip(payment){
+let tips = [];
+let finalBills = [];
 
+function calcTip(bill){
+
+    if(bill <= 50){
+        const tip = bill * 0.2;
+        const totalBill = bill * 1.2;
+        tips.push(tip);
+        finalBills.push(totalBill);
+    }
+    else if(bill > 50 && bill <= 200) {
+        const tip = bill * 0.15;
+        const totalBill = bill * 1.15;
+        tips.push(tip);
+        finalBills.push(totalBill);
+    }
+    else {
+        const tip = bill * 0.10;
+        const totalBill = bill * 1.10;
+        tips.push(tip);
+        finalBills.push(totalBill);
+    }
 }
+
+calcTip(124);
+calcTip(48);
+calcTip(268);
+console.log('tips', tips);
+console.log('total bills', finalBills)
+
